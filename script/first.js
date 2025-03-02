8a9f8f2fe3762f499aba801b188eb81845a49b01
Niloy-Uthso <niloyuthso16@gmail.com>
2025-03-02 21:06:23 +0600

    second commit

--- script/first.js
+++ script/first.js
@@ -1,7 +1,7 @@
 let coun=0;
 document.getElementById('Completed1').addEventListener('click',function(){
      
-    alert('vul')
+    alert('Board updated Successfully')
  let decrease= parseFloat(document.getElementById('decrease').innerText) 
  let increase=parseFloat(document.getElementById('increase').innerText)
  decrease--
@@ -9,27 +9,30 @@ document.getElementById('Completed1').addEventListener('click',function(){
  document.getElementById('decrease').innerText=decrease
  document.getElementById('increase').innerText=increase
   
-
+ const now = new Date();
+    const timeString = now.toLocaleTimeString(); 
+     
 
 const mainSpan=document.getElementById('adding-span')
 const addingSpan=document.createElement('span')
 
 addingSpan.innerHTML=`
-     You have Complete The Task <br> Add Dark Mode at 12:48:15 PM
+     You have Complete The Task <br>Fix Mobile Button Issue at: ${timeString}
 `
-addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm', 'text-blue-500')
+addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm', 'max-w-[200px]','mb-3','pl-2','rounded-lg'   )
+addingSpan.style.backgroundColor = 'rgba(244, 247, 255, 1)';
 mainSpan.appendChild(addingSpan)
 
  this.disabled = true;
  coun++
  if(coun===6)
-    alert('You have done all the task')
+    alert('Congrates!!!You have completed all the current task')
     
 })
 
 document.getElementById('Completed2').addEventListener('click',function(){
      
-    alert('vul')
+    alert('Board updated Successfully')
  let decrease= parseFloat(document.getElementById('decrease').innerText) 
  let increase=parseFloat(document.getElementById('increase').innerText)
  decrease--
@@ -41,23 +44,26 @@ document.getElementById('Completed2').addEventListener('click',function(){
 
 const mainSpan=document.getElementById('adding-span')
 const addingSpan=document.createElement('span')
-
+const now = new Date();
+    const timeString = now.toLocaleTimeString(); 
+    
 addingSpan.innerHTML=`
-     You have Complete The Task <br> Add Dark Mode at 12:48:15 PM
+     You have Complete The Task <br> Add Dark Mode at:${timeString}
 `
-addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm', 'text-blue-500')
+addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm','mb-3','pl-2','rounded-lg' )
+addingSpan.style.backgroundColor = 'rgba(244, 247, 255, 1)';
 mainSpan.appendChild(addingSpan)
 
  this.disabled = true;
  coun++
  if(coun===6)
-    alert('You have done all the task')
+    alert('Congrates!!!You have completed all the current task')
     
 })
 
 document.getElementById('Completed3').addEventListener('click',function(){
      
-    alert('vul')
+    alert('Board updated Successfully')
  let decrease= parseFloat(document.getElementById('decrease').innerText) 
  let increase=parseFloat(document.getElementById('increase').innerText)
  decrease--
@@ -69,23 +75,26 @@ document.getElementById('Completed3').addEventListener('click',function(){
 
 const mainSpan=document.getElementById('adding-span')
 const addingSpan=document.createElement('span')
-
+const now = new Date();
+    const timeString = now.toLocaleTimeString(); 
+    
 addingSpan.innerHTML=`
-     You have Complete The Task <br> Add Dark Mode at 12:48:15 PM
+     You have Complete The Task <br>Optimize Home page at:${timeString}
 `
-addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm', 'text-blue-500')
+addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm','max-w-[200px]','mb-3','pl-2','rounded-lg' )
+addingSpan.style.backgroundColor = 'rgba(244, 247, 255, 1)';
 mainSpan.appendChild(addingSpan)
 
  this.disabled = true;
  coun++
  if(coun===6)
-    alert('You have done all the task')
+    alert('Congrates!!!You have completed all the current task')
     
 })
 
 document.getElementById('Completed4').addEventListener('click',function(){
      
-    alert('vul')
+    alert('Board updated Successfully')
  let decrease= parseFloat(document.getElementById('decrease').innerText) 
  let increase=parseFloat(document.getElementById('increase').innerText)
  decrease--
@@ -97,23 +106,26 @@ document.getElementById('Completed4').addEventListener('click',function(){
 
 const mainSpan=document.getElementById('adding-span')
 const addingSpan=document.createElement('span')
-
+const now = new Date();
+    const timeString = now.toLocaleTimeString(); 
+    
 addingSpan.innerHTML=`
-     You have Complete The Task <br> Add Dark Mode at 12:48:15 PM
+     You have Complete The Task <br>Add new emoji  🤲  at:${timeString}
 `
-addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm', 'text-blue-500')
+addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm','mb-3','pl-2','rounded-lg' )
+addingSpan.style.backgroundColor = 'rgba(244, 247, 255, 1)';
 mainSpan.appendChild(addingSpan)
 
  this.disabled = true;
  coun++
  if(coun===6)
-    alert('You have done all the task')
+    alert('Congrates!!! You have completed all the current task')
     
 })
 
 document.getElementById('Completed5').addEventListener('click',function(){
      
-    alert('vul')
+    alert('Board updated Successfully')
  let decrease= parseFloat(document.getElementById('decrease').innerText) 
  let increase=parseFloat(document.getElementById('increase').innerText)
  decrease--
@@ -125,23 +137,27 @@ document.getElementById('Completed5').addEventListener('click',function(){
 
 const mainSpan=document.getElementById('adding-span')
 const addingSpan=document.createElement('span')
+const now = new Date();
+    const timeString = now.toLocaleTimeString(); 
+    
 
 addingSpan.innerHTML=`
-     You have Complete The Task <br> Add Dark Mode at 12:48:15 PM
+     You have Complete The Task <br>Integrate OpenAI API at:${timeString}
 `
-addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm', 'text-blue-500')
+addingSpan.classList.add('inline-block','vanish', 'w-fit', 'break-words', 'text-sm','max-w-[200px]','mb-3','pl-2','rounded-lg')
+addingSpan.style.backgroundColor = 'rgba(244, 247, 255, 1)';
 mainSpan.appendChild(addingSpan)
 
  this.disabled = true;
  coun++
  if(coun===6)
-    alert('You have done all the task')
+    alert('Congrates!!! You have completed all the current task')
     
 })
 
 document.getElementById('Completed6').addEventListener('click',function(){
      
-    alert('vul')
+    alert('Board updated Successfully')
  let decrease= parseFloat(document.getElementById('decrease').innerText) 
  let increase=parseFloat(document.getElementById('increase').innerText)
  decrease--
@@ -153,23 +169,26 @@ document.getElementById('Completed6').addEventListener('click',function(){
 
 const mainSpan=document.getElementById('adding-span')
 const addingSpan=document.createElement('span')
-
+const now = new Date();
+    const timeString = now.toLocaleTimeString(); 
+     
 addingSpan.innerHTML=`
-     You have Complete The Task <br> Add Dark Mode at 12:48:15 PM
+     You have Complete The Task <br>Improve Job searching at:${timeString}
 `
 
-addingSpan.classList.add('inline-block','vanish','w-fit', 'break-words', 'text-sm', 'text-blue-500')
+addingSpan.classList.add('inline-block','vanish','w-fit', 'break-words', 'text-sm','max-w-[200px]','mb-3','pl-2','rounded-lg' )
+addingSpan.style.backgroundColor = 'rgba(244, 247, 255, 1)';
 mainSpan.appendChild(addingSpan)
 
  this.disabled = true;
  coun++
  if(coun===6)
-    alert('You have done all the task')
+    alert('Congrates!!! You have completed all the current task')
     
 })
 
 document.getElementById('clear').addEventListener('click',function(){
-    console.log('ebar')
+     
 
     const loop= document.getElementsByClassName('vanish')
 
@@ -204,7 +223,29 @@ changeColorBtn.addEventListener('click', () => {
     colorBox.style.backgroundColor = randomColor;
   });
 
+
+  const now = new Date();
+   
+   const dateString = now.toLocaleDateString([], { year: 'numeric', month: 'long', day: 'numeric' });
+
  
+   const dayString = now.toLocaleDateString([], { weekday: 'long' });
+
+ 
+    console.log('out')
+    const mainP=document.getElementById('date')
+    const addingP=document.createElement('p')
+
+     
+    addingP.innerHTML=`
+     ${dayString},<br>  <strong>${dateString}</strong>
+    `
+    
+     
+    mainP.appendChild(addingP)
+ 
+
+
 
 
 
